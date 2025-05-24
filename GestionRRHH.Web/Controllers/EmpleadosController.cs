20ad53c11e24e5279c42e170bef0f92a635eb2d4
Kenny Saenz <saenzk031@gmail.com>
2025-05-23 20:04:43 -0600

    ADD: Vista de empleados

--- GestionRRHH.Web/Controllers/EmpleadosController.cs
+++ GestionRRHH.Web/Controllers/EmpleadosController.cs
@@ -1,8 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
-using GestionRRHH.Web.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using GestionRRHH.Web.Data;
+using GestionRRHH.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestionRRHH.Web.Controllers;
+
 [Authorize]
 public class EmpleadosController : Controller
 {
@@ -15,7 +19,90 @@ public class EmpleadosController : Controller
 
     public async Task<IActionResult> Index()
     {
-        var empleados = _context.Empleados.ToList();
+        var empleados = await _context.Empleados
+            .Include(e => e.Departamento)
+            .Include(e => e.Puesto)
+            .ToListAsync();
         return View(empleados);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        await LoadDropdowns();
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Empleado empleado)
+    {
+        if (ModelState.IsValid)
+        {
+            empleado.FechaCreacion = DateTime.Now;
+            empleado.FechaModificacion = DateTime.Now;
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "Empleado creado exitosamente.";
+            return RedirectToAction(nameof(Index));
+        }
+        
+        await LoadDropdowns();
+        return View(empleado);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var empleado = await _context.Empleados.FindAsync(id);
+        if (empleado == null)
+            return NotFound();
+        
+        await LoadDropdowns();
+        return View(empleado);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Empleado empleado)
+    {
+        if (id != empleado.Id)
+            return NotFound();
+        
+        if (ModelState.IsValid)
+        {
+            empleado.FechaModificacion = DateTime.Now;
+            _context.Update(empleado);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "Empleado actualizado exitosamente.";
+            return RedirectToAction(nameof(Index));
+        }
+        
+        await LoadDropdowns();
+        return View(empleado);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var empleado = await _context.Empleados.FindAsync(id);
+        if (empleado != null)
+        {
+            empleado.Estado = "inactivo";
+            empleado.FechaModificacion = DateTime.Now;
+            _context.Update(empleado);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "Empleado desactivado exitosamente.";
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    private async Task LoadDropdowns()
+    {
+        ViewBag.Departamentos = new SelectList(
+            await _context.Departamentos.ToListAsync(), 
+            "Id", "Nombre");
+        
+        ViewBag.Puestos = new SelectList(
+            await _context.Puestos.ToListAsync(), 
+            "Id", "Nombre");
     }
 }
